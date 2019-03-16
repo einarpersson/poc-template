59d24dadfb4e7191a70b2c528066bdcaaaa1ed55
Einar Persson <me@einarpersson.com>
2019-03-16 13:19:32 +0100

    Add json middleware

--- api/server.js
+++ api/server.js
@@ -5,9 +5,15 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res, next) => {
   res.send('hello from api')
+})
+
+app.post('/', ({ body }, res, next) => {
+  console.log(body)
+  res.send('ok')
 })
 
 app.listen(process.env.API_PORT, () => {
